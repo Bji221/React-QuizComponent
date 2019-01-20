bfcadfecbf53aebd502734679c2572ed213bfb40
basrw3 <balaji.baskaran@cchmc.org>
2019-01-19 20:46:22 -0500

    handle quiz reset

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,10 @@ class Quiz extends Component {
         super(props);
         this.state = {quiz_position : 1};
     }
+    
+    handleResetClick() {
+        this.setState((state) => {return {quiz_position : 1}});
+    }
 
     showNextQuestion() {
         this.setState((state) => {
@@ -20,7 +24,7 @@ class Quiz extends Component {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         //console.log(isQuizEnd);
         return <div>
-            {isQuizEnd ? <QuizEnd /> :
+            {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> :
             <QuizQuestion  showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
             </div>;
     }
